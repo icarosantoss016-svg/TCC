4ab3c0f343dce1059c0220b332d36dd84ed2eb4b
Ícaro Santos <icarosantoss016@gmail.com>
2026-08-27 17:16:34 -0300

    Correção de Usuario Controller, e Criação de Empresa Contoreller

---
+++ controllers/empresaContoller.js
@@ -0,0 +1,118 @@
+const Empresa = require ('../models/empresa')
+
+exports.criarEmpresa= async(req,res)=>{
+    try {
+        const {nome,cnpj} = req.body
+
+        if(!nome|| typeof nome!=='string'|| !nome.trim()){
+            return res.status(400).json({error:'Nome é obrigatorio.'})
+        }
+        if(!cnpj|| typeof cnpj!=='string'|| !cnpj.trim()){
+            return res.status(400).json({error:'CNPJ é obrigatorio.'})
+
+        }
+
+        const novaEmpresa = await Empresa.create({
+            nome,
+            cnpj
+        })
+
+        return res.status(201).json({
+            mensage:'Empresa criada com sucesso',
+            Empresa: novaEmpresa
+        })
+    } catch (error) {
+        console.error('Erro ao criar empresa:', error)
+        return res.status(500).json({error:'Erro interno do servidor ao criar empresa..'})
+    }
+}
+
+exports.listarEmpresas = async(req,res)=>{
+    try {
+        const empresas = await Empresa.findAll()
+
+        return res.status(200).json(empresas)
+    } catch (error) {
+        console.error('Erro ao listar empresas', error)
+        return res. status(500).json({error:'Erro interno ao listar empresas.'})
+        
+    }
+}
+
+exports.buscarEmpresasId = async (req,res) =>{
+    try {
+        const empresa = await Empresa.findByPk(req.params.id)
+
+        if(!empresa){
+            return res.status(404).json({error:'Empresa não encotrada.'})
+        }
+
+        return res.status(200).json(empresa)
+    } catch (error) {
+        console.error('Erro ao buscar empresa', error)
+        return res. status(500).json({error:'Erro interno ao buscar empresa.'})
+    }
+}
+
+exports.deletarEmpresa = async (req,res)=>{
+    try {
+        const linhasDeletadas = await Empresa.destroy({
+            where:{id:req.params.id}
+        })
+
+        if(linhasDeletadas===0){
+            return res.status(404).json({error:'Empresa não encotrada.'})
+        }
+    } catch (error) {
+        console.error("Erro ao deletar a empresa:", error);
+        return res.status(500).json({ error: "Erro interno ao deletar empresa." })
+    }
+}
+
+exports.atualizarEmpresa = async (req, res) => {
+      try {
+        const empresa = await Empresa.findByPk(req.params.id)
+        const {nome,cnpj} = req.body
+
+        if(!nome|| typeof nome!=='string'|| !nome.trim()){
+            return res.status(400).json({error:'Nome é obrigatorio.'})
+        }
+        if(!cnpj|| typeof cnpj!=='string'|| !cnpj.trim()){
+            return res.status(400).json({error:'CNPJ é obrigatorio.'})
+
+        }
+
+        await empresa.update({
+            nome:nome,
+            cnpj:cnpj
+        })
+
+        return res.status(201).json({
+            mensage:'Empresa atualizada com sucesso',
+            Empresa:empresa
+        })
+    } catch (error) {
+        console.error('Erro ao atualizar empresa:', error)
+        return res.status(500).json({error:'Erro interno do servidor ao atualizar.'})
+    }    
+}
+
+exports.criarEmpresaAdmin = async()=>{
+    try {
+        const empresaAdminExiste = await Empresa.findOne({
+            where:{cnpj:'00000000000000'}
+        })
+        
+        if(!empresaAdminExiste){
+            await Empresa.create({
+                nome:'SafeZone',
+                cnpj:'00000000000000'
+            })
+        }
+
+        console.log('Empresa admin padrão criada (Nome:SafeZone, cnpj:00000000000000  )')
+    } catch (error) {
+        console.error( 'Erro ao criar empresa padrão:', error)    
+
+    }
+}
--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt")
 
 exports.criarUsuario = async (req, res) => {
   try {
-    const {login, senha, perfil} = req.body
+    const {login, senha, perfil, id_empresa} = req.body
 
     if(!login|| typeof login!=='string'||!login.trim()){
       return res.status(400).json({error:'Login é obrigatório.'})
@@ -17,20 +17,33 @@ exports.criarUsuario = async (req, res) => {
       return res.status(400).json({error:'Perfil é obrigatória.'})
     }
 
+    if(!id_empresa||isNaN(id_empresa)){
+      return res.status(400).json({error:'Id da empresa é obrigatório.'})
+    }
+
+
     const perfilFormatado = perfil.trim().toUpperCase()
 
     const perfisPermitidos = ['ADMIN','ADM_EMPRESA','USUARIO']
     if(!perfisPermitidos.includes(perfilFormatado)){
       return res.status(400).json({error: `Perfil inválido. Use: ${perfisPermitidos.join(', ')}`})
     }
+   
+    const empresa = await findOne({where:{id_empresa:id_empresa}})
 
+    if(!empresa||empresa===null|| empresa===undefined){
+      return res.status(404).json({error:'Empresa não localizada pelo Id.'})
+    }
+    
+    
     const salt = await bcrypt.genSalt(10)
     const senhaCriptografada = await bcrypt.hash(senha, salt)
 
     const novoUsuario = await Usuario.create({
       login:login.trim(),
       senha:senhaCriptografada,
-      perfil:perfilFormatado
+      perfil:perfilFormatado,
+      id_empresa:empresa
     })
 
     return res.status(201).json({
@@ -130,7 +143,8 @@ exports.criarAdminPadrao = async ()=>{
       await Usuario.create({
         login:'admin',
         senha:senhaHash,
-        perfil:'ADMIN'
+        perfil:'ADMIN',
+        id_empresa:1
       })
       console.log('Usuário admin padrão criado(Login:admin / Senha:admin123)')      
     }
