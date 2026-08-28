1a06ae6a8a14a12088b2be3a2669d8825027d722
Ícaro Santos <icarosantoss016@gmail.com>
2026-08-28 14:03:55 -0300

    Fix(regraEpiRouter): Correção de sintaxe e limpeza de código

--- routers/regraEpiRoutes.js
+++ routers/regraEpiRoutes.js
@@ -2,7 +2,6 @@ const express= require('express')
 const router = express.Router()
 const regraEpiController= require('../controllers/regraEpiController')
 const authMiddleware = require('../middleware/authMiddleware')
-const { route } = require('./usuarioRoutes')
 
 router.post('/api/criarRegra',authMiddleware,regraEpiController.criarRegraEpi)
 router.get('/api/listarRegra',authMiddleware,regraEpiController.listarRegrasEpi)
