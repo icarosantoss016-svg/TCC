2f825effe31c01b599fdd41e557fb8adf343c1f9
Ícaro Santos <icarosantoss016@gmail.com>
2026-08-28 01:15:01 -0300

    Fix: Correção de sintaxe na definição de endpoints.

--- routers/regraEpiRoutes.js
+++ routers/regraEpiRoutes.js
@@ -2,9 +2,12 @@ const express= require('express')
 const router = express.Router()
 const regraEpiController= require('../controllers/regraEpiController')
 const authMiddleware = require('../middleware/authMiddleware')
+const { route } = require('./usuarioRoutes')
 
 router.post('/api/criarRegra',authMiddleware,regraEpiController.criarRegraEpi)
 router.get('/api/listarRegra',authMiddleware,regraEpiController.listarRegrasEpi)
 router.get('/api/buscarRegra/:id', authMiddleware, regraEpiController.buscarRegraEpiId)
 router.put('/api/atulaizarRegra/:id',authMiddleware, regraEpiController.atualizarRegraEpi)
 router.delete('/api/deletarRegra/:id',authMiddleware,regraEpiController.deletarRegraEpi)
+
+module.exports= router
