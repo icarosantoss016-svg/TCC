01462b628aba412d4c745fce9cb5e9110fa8c92d
Ícaro Santos <icarosantoss016@gmail.com>
2026-08-28 00:42:19 -0300

    Feat(usuarioRoutes): inclusão de endpoints

--- routers/usuarioRoutes.js
+++ routers/usuarioRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController')
+const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/api/usuario', usuarioController.criarUsuario)
+router.post('/api/usuario', authMiddleware, usuarioController.criarUsuario)
+router.get('/api', authMiddleware,usuarioController.listarUsuarios)
+router.get('/api/buscarUsuario/:id_usuario', authMiddleware,usuarioController.buscarUsuarioId)
+router.put('/api/atualizarSenha/:id_usuario',authMiddleware,usuarioController.atualizarSenha)
+router.delete('/api/deletarUsuario/:id_usuario',authMiddleware,usuarioController.deletarUsuario)
 
 module.exports = router
